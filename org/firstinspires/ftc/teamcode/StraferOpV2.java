63ab5b401a59b60cf25f51e796db2d985f4d726a
Cory Schmitz <cory@otis8.com>
2024-10-21 21:12:18 -0500

    I added a comment to the code

--- org/firstinspires/ftc/teamcode/StraferOpV2.java
+++ org/firstinspires/ftc/teamcode/StraferOpV2.java
@@ -1,5 +1,10 @@
 package org.firstinspires.ftc.teamcode;
 
+/*
+ * This is strafer code
+ *
+ */
+
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
